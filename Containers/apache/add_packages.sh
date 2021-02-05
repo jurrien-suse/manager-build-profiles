f7fc04d4810c39331891ce5f3e2e27c29953bd21
jurrien-suse <58264730+jurrien-suse@users.noreply.github.com>
2021-02-05 20:11:17 +0100

    Update add_packages.sh

--- Containers/apache/add_packages.sh
+++ Containers/apache/add_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-set -e
+#set -e
 
-zypper --non-interactive --gpg-auto-import-keys ref
+#zypper --non-interactive --gpg-auto-import-keys ref
 
 #zypper --non-interactive in aaa_base aaa_base-extras net-tools timezone vim less sudo tar python python-xml apache2 apache2-prefork
 zypper --non-interactive in apache2 apache2-prefork
